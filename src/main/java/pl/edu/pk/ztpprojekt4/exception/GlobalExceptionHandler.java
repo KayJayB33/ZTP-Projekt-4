bfcb915d339f0c6314f54896643214d4443b6fa6
Krzysztof Bętkowski <krzysztof.betkowski00@gmail.com>
2023-12-31 14:30:19 +0100

    Fix Exception Handler to handle all Exceptions

--- src/main/java/pl/edu/pk/ztpprojekt4/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/edu/pk/ztpprojekt4/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package pl.edu.pk.ztpprojekt4.exception;
 
 import org.springframework.ui.Model;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public String handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, Model model) {
+    public String handleHttpRequestMethodNotSupportedException(Exception ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error";
     }
